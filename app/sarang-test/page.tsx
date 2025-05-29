f5606874d556672bda459fdd41fc7e3df603204f
seongEun95 <dsa02070@naver.com>
2025-05-29 17:35:19 +0900

    초기 커밋

---
+++ app/sarang-test/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function SarangTest() {
+  return (
+    <div className="text-red-500 flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">SarangTest</h1>
+      <Button variant="secondary" className="mt-2 text-red-500">
+        Click me
+      </Button>
+    </div>
+  );
+}
