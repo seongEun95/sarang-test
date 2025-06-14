fd0e48f5b5e721a9883ab92b3a2951d62de7895d
seongEun95 <dsa02070@naver.com>
2025-06-14 21:25:58 +0900

    feat: vercel analytics 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Noto_Sans_KR, Black_Han_Sans } from "next/font/google";
 import "./globals.css";
 import { ScoreProvider } from "./sarang-test/context/ScoreContext";
 import GoogleAnalytics from "@/lib/GoogleAnalytics";
+import { Analytics } from '@vercel/analytics/next';
 
 const notoSans = Noto_Sans_KR({
   variable: "--font-noto-sans",
@@ -39,6 +40,7 @@ export default function RootLayout({
       <body className={`${notoSans.variable} ${blackHanSans.variable} antialiased`}>
         <div className="font-sans w-full flex flex-col items-center justify-center bg-[url('/sarang-bg.png')] bg-cover bg-center">
           <ScoreProvider>{children}</ScoreProvider>
+          <Analytics />
         </div>
       </body>
     </html>
