597db77092870725955278e90d861b53fdc0dcfe
seongEun95 <dsa02070@naver.com>
2025-06-14 21:12:16 +0900

    feat: google analytics 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_KR, Black_Han_Sans } from "next/font/google";
 import "./globals.css";
 import { ScoreProvider } from "./sarang-test/context/ScoreContext";
+import GoogleAnalytics from "@/lib/GoogleAnalytics";
 
 const notoSans = Noto_Sans_KR({
   variable: "--font-noto-sans",
@@ -32,6 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+      )}
       <body className={`${notoSans.variable} ${blackHanSans.variable} antialiased`}>
         <div className="font-sans w-full flex flex-col items-center justify-center bg-[url('/sarang-bg.png')] bg-cover bg-center">
           <ScoreProvider>{children}</ScoreProvider>
